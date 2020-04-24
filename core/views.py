8e1b14766b3a069f73b1b45f67cf1ea3217c8d92
Joao Gabriel <joao.gsilva@loggi.com>
2020-04-24 17:03:34 -0300

    include about , update ligthbox

--- core/views.py
+++ core/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 
 from listings.models import Listing
+from realtors.models import Realtor
 
 def index(request):
-    listing = Listing.order_by('-list_date').filter(is_published=True)[:3]
+    listing = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
     context = {
         'listings': listing
     }
@@ -11,4 +12,13 @@ def index(request):
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)
+
+    context = { 
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+
+    return render(request, 'pages/about.html', context)
